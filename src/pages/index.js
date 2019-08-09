82393b96c1644305202ae15e021b0b1b7f86fcff
CraigCharlton <craig.charlton@bedegaming.com>
2019-08-09 15:35:00 +0100

    Image added

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,9 @@
-import React from "react"
+import React from 'react';
+import Image from '../images/craig.jpg';
 
-export default () => <div>Craig Charlton</div>
+export default () => (
+  <div>
+    <h1>Craig Charlton</h1>
+    <img src={Image} alt="me" width="50rem" />
+  </div>
+);
