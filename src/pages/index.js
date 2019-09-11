a7cb9bbf5945760dcec6f029e63ba57a5560b2bb
CraigCharlton <craig.charlton@bedegaming.com>
2019-09-11 10:41:15 +0100

    Added blog link and basic content/styles

--- src/pages/index.js
+++ src/pages/index.js
@@ -19,12 +19,21 @@ export default () => {
         </li>
         <li className="page page-about" id="about">
           <h2>About</h2>
+          <p>I'm Craig!</p>
         </li>
         <li className="page page-projects" id="projects">
           <h2>Projects</h2>
+          <Link to="https://github.com/CraigCharlton/Craig-Web">This Site!</Link>
         </li>
         <li className="page page-contact" id="contact">
           <h2>Contact</h2>
+          <Link to="https://www.linkedin.com/in/charlton87/">LinkedIn</Link>
+        </li>
+        <li className="page page-blog" id="blog">
+          <h2>Blog</h2>
+          <div className="blogs-container">
+            here!
+          </div>
         </li>
       </ul>
     </Layout>
